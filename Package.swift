1b3ecf7317aee229609f8c2a76ef20e5d93bbf6e
Alex G <123711305+alexgmax@users.noreply.github.com>
2025-12-03 21:14:29 +0700

    Fix info.plist issues

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "PremiumAdsAdapter",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v15),
     ],
     products: [
         .library(
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PremiumAdsAdapter",
-            url: "https://raw.githubusercontent.com/premium-ads/adapter-ios-spm/refs/heads/main/PremiumAdsAdapter-2.2.6-SPM.xcframework.zip",
-            checksum: "f6d9e30f25cef752dfd0e2243dcec4c74aa679fed7e9bc75efb395d1e31b1116"
-        )
+            path: "Frameworks/PremiumAdsAdapter.xcframework"
+        ),
     ]
 )
