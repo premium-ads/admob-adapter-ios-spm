b930eb6bc75f3893bee4d93ff500c1f66578097a
Alex G <123711305+alexgmax@users.noreply.github.com>
2025-12-03 14:00:28 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PremiumAdsAdapter",
-            url: "https://raw.githubusercontent.com/premium-ads/premiumads-adapter-ios/refs/heads/main/PremiumAdsAdapter-2.2.6.xcframework.zip",
+            url: "https://raw.githubusercontent.com/premium-ads/adapter-ios-spm/refs/heads/main/PremiumAdsAdapter-2.2.6.xcframework.zip",
             checksum: "89ca9d68476a6da87ebc97ec08b12aef1f29281068ee07a302a76bf17bec64fb"
         )
     ]
